b132f8a2242c036f55aebbacbd3fe263f8528ee1
tldhsdl22 <tldhsdl22@gmail.com>
2019-04-24 21:22:41 +0900

    1_first

---
+++ GGBB_re/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GGBB_re
+//
+//  Created by 송시온 on 2019. 4. 24..
+//  Copyright © 2019년 송시온. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ GGBB_reTests/GGBB_reTests.swift
@@ -0,0 +1,36 @@
+//
+//  GGBB_reTests.swift
+//  GGBB_reTests
+//
+//  Created by 송시온 on 2019. 4. 24..
+//  Copyright © 2019년 송시온. All rights reserved.
+//
+
+import XCTest
+@testable import GGBB_re
+
+class GGBB_reTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
